c75ed27a9ad3b7d52cad3b01958abaa72000f1d9
John Alex <bloodroserecords@gmail.com>
2025-03-27 11:19:29 -0400

    Adding mversion

--- version/mversion.c
+++ version/mversion.c
@@ -4,15 +4,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/stat.h>
-#include "version.h"
 #include "../src/file.c"
+#include "version.h"
 
 #define version_path ".git/version.h"
 #define script_path ".git/hooks/post-commit"
 const char gitperm[] = "chmod +x " script_path;
 const char gitscript[] = "#!/bin/bash\nmversion commit\n";
 const char gitadd[] = "git add -f " version_path;
-const char gitlink[] = "ln -s ./" version_path " ./%s";
+const char gitlink[] = "ln -s $(realpath --relative-to=$(dirname %s) "version_path ") %s";
 
 const char strings_help[] = "mversion helps manage the version-number string\n\
 run \"mversion init\" in a git repository to set up the git hook\n\
@@ -28,30 +28,31 @@ const char TAGS[][8] = {
     [PATCH] = "PATCH"
 };
 
+uint32_t vnums[3];
 char cmdbuf[80];
 
 // #define strings_version "##.##.##"
-void getVersion(uint32_t *nums) {
+void getVersion() {
     char buffer[80];
     int r = file_read(version_path, buffer, sizeof(buffer)); 
-    if ( r <= 0 ) { nums[0] = -1; nums[1] = -1; nums[2] = -1; return; }
+    if ( r <= 0 ) { vnums[0] = -1; vnums[1] = -1; vnums[2] = -1; return; }
     char *header = (char *)(buffer + 25);
-    nums[0]  = strtol(header, &header, 10);
-    nums[1]  = strtol(header++, &header, 10);
-    nums[2]  = strtol(header++, &header, 10);
+    vnums[0]  = strtol(header, &header, 10);
+    vnums[1]  = strtol(header++, &header, 10);
+    vnums[2]  = strtol(header++, &header, 10);
 }
 
-int writeVersion(uint32_t *nums) {
+int writeVersion() {
     static char buffer[80];
-    sprintf(buffer, "#define strings_version \"%d.%d.%d\"", nums[0], nums[1], nums[2]);
+    sprintf(buffer, "#define strings_version \"%d.%d.%d\"\n", vnums[0], vnums[1], vnums[2]);
     return file_write(version_path, buffer, sizeof(buffer));
 }
 
-void updateVersion(uint32_t *nums, char *commit) {
+void updateVersion(char *commit) {
     int index = 2; // minor by default
     int i; for (i=0;i<3;i++) { if (strncmp(commit, TAGS[i], 5)==0) { index = i; break; } }
     printf( TAGS[i] );
-    nums[i] = nums[i] + 1;
+    vnums[i] = vnums[i] + 1;
 }
 
 char *commit_message() {
@@ -63,16 +64,15 @@ char *commit_message() {
     fclose(fp); return commit_msg;
 }
 
-void initVersion(uint32_t *nums){
+void initVersion(){
     int i; for(i=0;i<3;i++) {
         printf("    %s > ", TAGS[i]);
         fgets(cmdbuf, sizeof(cmdbuf), stdin);
-        nums[i] = strtol(cmdbuf, NULL, 10);
+        vnums[i] = strtoll(cmdbuf, NULL, 10);
     }
 }
 
 int main(int argc, char **argv) {
-    uint32_t version_numbers[3] = {0};
     if ( argc == 1 ) { printf(strings_help); exit(0); }
     else if ( strcmp(argv[1], "init")==0 ) {
         if (!file_exists(".git")) { fprintf(stderr, "cwd is not a git repository\n"); exit(1); }
@@ -80,42 +80,42 @@ int main(int argc, char **argv) {
         if ( w <= 0 ) { fprintf(stderr, "unable to write to %s\n", script_path); exit(1); }
         int s = system(gitperm); if ( s != 0 ) { fprintf(stderr, "Unable to set write permissions for " script_path "\n"); exit(1); }
         if ( file_exists(version_path) ) { printf("Warning: `version.h` already exists, overwriting\n"); } else { printf("Initialized version as 0.0.0\n"); }
-        writeVersion(version_numbers);
+        writeVersion();
     }
     else if ( strcmp(argv[1], "set")==0) {
         if (!file_exists(".git")) { fprintf(stderr, "cwd is not a git repository\n"); exit(1); }
         printf("Enter the new version number.\n");
-        initVersion(version_numbers);
-        writeVersion(version_numbers);
+        initVersion();
+        writeVersion();
     }
     else if ( strcmp(argv[1], "commit")==0 ) {
         if (!file_exists(".git")) { fprintf(stderr, "cwd is not a git repository\n"); exit(1); }
         if (!file_exists(version_path)) { fprintf(stderr, "Version information missing, please run `mversion set`\n"); exit(1); }
         else {
-            getVersion(version_numbers);
-            if ( version_numbers[0] == -1 ) {
+            getVersion();
+            if ( vnums[0] == -1 ) {
                 printf("Invalid version information found.\n");
-                initVersion(version_numbers);
-                writeVersion(version_numbers);
+                initVersion();
+                writeVersion();
             }
             char *message = commit_message();
-            printf("... updating version %d.%d.%d -> ", version_numbers[0], version_numbers[1], version_numbers[2]);
-            updateVersion(version_numbers, message);
-            printf("%d.%d.%d\n", version_numbers[0], version_numbers[1], version_numbers[2]);
-            writeVersion(version_numbers);
+            printf("... updating version %d.%d.%d -> ", vnums[0], vnums[1], vnums[2]);
+            updateVersion(message);
+            printf("%d.%d.%d\n", vnums[0], vnums[1], vnums[2]);
+            writeVersion();
         }
     }
     else if ( argv[1][0] == '-' ) {
         char tag = argv[1][1]; if ( tag == '-' ) { tag = argv[1][2]; }
         switch ( tag ) {
             default:  fprintf(stderr, "Unknown command %s\n", argv[1]); exit(1);
-            case 'v': printf("mversion v %s\n", strings_version); exit(0);
             case 'h': printf(strings_help); exit(0);
+            case 'v': printf(strings_version); exit(0);
         }
     }
     else {
         printf("... creating symlink %s to .git/version.h\n", argv[1]);
-        sprintf(cmdbuf, gitlink, argv[1]);
+        sprintf(cmdbuf, gitlink, argv[1], argv[1]);
         int s = system(cmdbuf);
         if ( s != 0 ) { fprintf(stderr, "failed to create symlink\n"); exit(1); }
     }
--- version/version.h
+++ version/version.h
@@ -1,1 +1,1 @@
-#define strings_version "0.0.0"
+../.git/version.h
